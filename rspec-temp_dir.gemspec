2ed4c2b537361f0ebdaf9f5c2ecdd97f3c082652
sue445 <sue445@sue445.net>
2014-06-15 00:22:14 +0900

    Setup coveralls

--- rspec-temp_dir.gemspec
+++ rspec-temp_dir.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake"
 
   spec.add_development_dependency "codeclimate-test-reporter"
+  spec.add_development_dependency "coveralls"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,9 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec/temp_dir'
 
