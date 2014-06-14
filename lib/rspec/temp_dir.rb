b4be8e0cc521d52627c90a2e9c428c524c90afb7
sue445 <sue445@sue445.net>
2014-06-14 23:30:55 +0900

    Initial commit

---
+++ lib/rspec/temp_dir.rb
@@ -0,0 +1,7 @@
+require "rspec/temp_dir/version"
+
+module Rspec
+  module TempDir
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/temp_dir_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rspec::TempDir do
+  it 'has a version number' do
+    expect(Rspec::TempDir::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
